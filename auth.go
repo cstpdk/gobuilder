85fb015145ea576d7c6b0401a9225b3f15eb07d2
mrob <mrob@itu.dk>
2013-12-12 20:59:13 +0100

    basic authentication

--- auth.go
+++ auth.go
@@ -3,18 +3,39 @@ package main
 import(
     "github.com/codegangsta/martini"
     "net/http"
+    "encoding/base64"
+    "strings"
+    "code.google.com/p/go.crypto/bcrypt"
+    "fmt"
 )
 
 /*
 Auth middel ware function for handling authentication and injecting User
 */
 func Auth(c martini.Context, w http.ResponseWriter, r *http.Request){
-    var u User = User{
-        Username:  "test",
-        Email: "test",
-        Role:  "admin",
+    auth := r.Header.Get("Authorization")
+    split := strings.SplitN(string(auth)," ",2)
+
+    data, err := base64.StdEncoding.DecodeString(split[1])
+
+
+    if err != nil {
+        http.Error(w, err.Error(), 500)
     }
-    c.Map(u)
+
+    info := strings.SplitN(string(data),":",2)
+
+    u, _ := getloginuser(info[0])
+    fmt.Printf("%#v", u)
+
+    crypterr := bcrypt.CompareHashAndPassword([]byte(u.Password),
+    []byte(info[1]))
+
+    if crypterr == nil{
+        c.Map(u)
+    }else{
+        http.Error(w, "Unauthorized", 401)
+    }
 }
 
 /*
